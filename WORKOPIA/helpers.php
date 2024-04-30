f93c0a63068eed68f00617e8dec8797d7e13adf4
Taehyun Sung <alexseong1004@gmail.com>
2024-05-01 07:46:26 +0800

    1/May 7am

--- WORKOPIA/helpers.php
+++ WORKOPIA/helpers.php
@@ -21,6 +21,7 @@ function loadView($name) {
 
     $viewPath = basePath("views/{$name}.view.php");
 
+
     if(file_exists($viewPath)) {
         require $viewPath;
     } else {
@@ -43,4 +44,28 @@ function loadPartial($name) {
     } else {
         echo "Partial '{$name} not found!'";
     }
+}
+
+/**
+ * Inspect a value(s)
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
